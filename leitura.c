9c27c8fbe2982603d2547f3b478fc6154c44a9ea
Pedro <pdparente@gmail.com>
2019-02-26 17:47:00 +0000

    leitura.c arrays dinamicos

--- leitura.c
+++ leitura.c
@@ -4,38 +4,69 @@
 
 int main() {
 	FILE *fp;
-	char* clientes[20000];
-	char* produtos[200000];
-	char* vendas[1000000];
+	char* *clientes = NULL;
+	char* *produtos = NULL;
+	char* * vendas = NULL;
 	char buffer[64];
-	int i = 0;
-	int j;
+	int i;
 	char* token;
+	char* niceBuffer;
+	int r=1;
+	int strcount = 0;
+	int numClientesReal;
+	int numProdutosReal;
+	int numVendas;
 
-	/*fp = fopen("./Ficheiros/Clientes.txt", "r");
+	fp = fopen("./Ficheiros/Clientes.txt", "r");
 	while(fgets(buffer, 64, fp)) {
-		clientes[i] = strdup(buffer);
-		i++;
+		niceBuffer = strtok(buffer, "\r\n");
+		clientes = (char**)realloc(clientes, (strcount + 1) * sizeof(char*));
+		clientes[strcount++] = strdup(niceBuffer);
 	}
 	fclose(fp);
+	numClientesReal = strcount;
+	strcount = 0;
 
 	fp = fopen("./Ficheiros/Produtos.txt", "r");
 	while(fgets(buffer, 64, fp)) {
-		produtos[i] = strdup(buffer);
-		i++;
+		niceBuffer = strtok(buffer, "\r\n");
+		produtos = (char**)realloc(produtos, (strcount + 1) * sizeof(char*));
+		produtos[strcount++] = strdup(niceBuffer);
 	}
 	fclose(fp);
-	*/
-	fp = fopen("./Ficheiros/Vendas_1M.txt", "r");
-	while(fgets(buffer, 64, fp)) {
-		vendas[i] = strdup(buffer);
-		i++;
-		token = strtok(buffer, " ");
-		printf( " %s\n", token );
-	}
+	numProdutosReal = strcount;
+	strcount = 0;
 
-	for(j=0;j<1000000;j++)
-		printf("%s", vendas[j]);
+
+	fp = fopen("./Ficheiros/Vendas_1M.txt", "r");
+	while(fgets(buffer, 64, fp)) { 
+		niceBuffer = strtok(buffer, "\r\n");
+		vendas = (char**)realloc(vendas, (strcount + 1) * sizeof(char*));
+		vendas[strcount++] = strdup(niceBuffer);
 		
+		token = strtok(niceBuffer, " ");
+		for(i=0;i<numProdutosReal;i++) {
+			if (strcmp(token,produtos[i]) == 0) {
+				r=0;
+				break;
+			} 
+		}
+		
+		token = strtok(NULL, " ");
+
+		//vendas[i] = strdup(token);
+		//i++;
+		//printf( "%s\n", token );
+	}
+	numVendas = strcount;
+
+	for(i=0;i<numVendas;i++)
+		printf("%s\n", vendas[i]);
+
+	for(i=0;i<numProdutosReal;i++)
+		printf("%s\n", produtos[i]);
+	
+	for(i=0;i<numClientesReal;i++)
+		printf("%s\n", clientes[i]);		
 	return 0;
 }
