834bbb2fe52a4b3691df9b2dd50a2d8ccc8050ba
AntonioG70 <antonio.carvalho9970@gmail.com>
2019-04-08 17:11:25 +0100

    #ez

--- leitura.h
+++ leitura.h
@@ -1,3 +1,9 @@
+/**
+ * @file leitura.h
+ * @author Grupo LI3 nº33
+ * @date 8 Abril 2019
+ * @brief Funções utilizadas pela leitura.
+ */
 #ifndef LEITURA_H
 #define LEITURA_H
 
@@ -6,10 +12,21 @@
 #include "faturacao.h"
 #include "gestaoFilial.h"
 
+/**
+ * @brief Função que saca os produtos de um ficheiro.
+ */
 void getProdutos(CAT_PRODUTOS p, char * fich);
 
+/**
+ * @brief Função que saca os clientes de um ficheiro.
+ */
 void getClientes(CAT_CLIENTES c, char * fich);
 
+/**
+ * @brief Função que saca as vendas de um ficheiro.
+ *
+ * @return Número de vendas
+ */
 int getVendas(FATGLOBAL fatGlobal, GESTAOFILIAL * gestFilial, CAT_PRODUTOS prod, CAT_CLIENTES client, char * fich);
 
 #endif
